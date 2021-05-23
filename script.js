6d7b2ebc981eeeff62b3fb7a5be6f52d614c1835
Rafael Medeiros <78233061+rafameyun@users.noreply.github.com>
2021-05-23 10:37:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({
+        pageLanguage: 'pt-BR', 
+        layout: google.translate.TranslateElement.InlineLayout.NEW
+    }, 'google_translate_element');
+  }
